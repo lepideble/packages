bf631971b0db6319d2c0123ed8b3a4da91b3ce7d
lepideble <147614625+lepideble@users.noreply.github.com>
2025-04-28 00:30:16 +0200

    Fix XML signing

--- bin/build.sh
+++ bin/build.sh
@@ -7,7 +7,11 @@ KEY=$(python3 -c "import config; print(config.signing_key)")
 for FILE in feeds/*.xml; do
     DEST="public/$(basename $FILE)"
 
-    cp "$FILE" "$DEST"
+    cat "$FILE" | sed '1a<?xml-stylesheet href="resources/feed.xsl" type="text/xsl"?>' > "$DEST"
 
-    0install run https://apps.0install.net/0install/0publish.xml --xmlsign --key=$KEY $DEST
+    SIGNATURE=$(gpg --default-key=$KEY --detach-sign < $DEST | base64)
+
+    echo "<!-- Base64 Signature" >> $DEST
+    echo "$SIGNATURE" >> $DEST
+    echo "-->" >> $DEST
 done
