bdb57e6cf1ad0c970e3784d255ad1751c2d454f4
lepideble <147614625+lepideble@users.noreply.github.com>
2025-05-04 09:52:44 +0200

    Add icons

--- bin/build.sh
+++ bin/build.sh
@@ -2,14 +2,21 @@
 
 cd $(dirname $(cd $(dirname $0) && pwd))
 
-KEY=$(python3 -c "import config; print(config.signing_key)")
+BASE_URL=$(python3 -c "import config; print(config.base_url)")
+SIGNING_KEY=$(python3 -c "import config; print(config.signing_key)")
+
+cp icons/*.png public/
 
 for FILE in feeds/*.xml; do
-    DEST="public/$(basename $FILE)"
+    NAME="$(basename "$FILE" .xml)"
+    DEST="public/$NAME.xml"
 
-    cat "$FILE" | sed '1a<?xml-stylesheet href="resources/feed.xsl" type="text/xsl"?>' > "$DEST"
+    cat "$FILE" \
+        | sed '1a<?xml-stylesheet href="resources/feed.xsl" type="text/xsl"?>' \
+        | sed "s#../icons/#$BASE_URL#" \
+        > "$DEST"
 
-    SIGNATURE=$(gpg --default-key=$KEY --detach-sign < $DEST | base64)
+    SIGNATURE=$(gpg --default-key=$SIGNING_KEY --detach-sign < $DEST | base64)
 
     echo "<!-- Base64 Signature" >> $DEST
     echo "$SIGNATURE" >> $DEST
