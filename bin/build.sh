113d89456d8154a6adccf8f1e7101ec6a9762825
lepideble <147614625+lepideble@users.noreply.github.com>
2025-05-09 13:12:24 +0200

    Update build script

--- bin/build.sh
+++ bin/build.sh
@@ -5,10 +5,23 @@ cd $(dirname $(cd $(dirname $0) && pwd))
 BASE_URL=$(python3 -c "import config; print(config.base_url)")
 SIGNING_KEY=$(python3 -c "import config; print(config.signing_key)")
 
+LAST_MESSAGE=$(cd public && git log -1 --pretty=%B)
+LAST_BUILT_COMMIT=${LAST_MESSAGE#"Update to "}
+
+CURRENT_COMMIT=$(git rev-parse HEAD)
+
+CHANGED_FEEDS=$(git diff --name-only $LAST_BUILT_COMMIT $CURRENT_COMMIT -- feeds)
+
+if [ -z "$CHANGED_FEEDS" ]; then
+    exit 0
+fi
+
 cp icons/*.png public/
 
-for FILE in feeds/*.xml; do
-    NAME="$(basename "$FILE" .xml)"
+for FILE in $CHANGED_FEEDS; do
+    NAME=${FILE#"feeds/"}
+    NAME=${NAME%".xml"}
+
     DEST="public/$NAME.xml"
 
     cat "$FILE" \
@@ -21,4 +34,8 @@ for FILE in feeds/*.xml; do
     echo "<!-- Base64 Signature" >> $DEST
     echo "$SIGNATURE" >> $DEST
     echo "-->" >> $DEST
+
+    (cd public && git add "$NAME.xml")
 done
+
+(cd public && git commit -m "Update to $CURRENT_COMMIT")
